55d5f593a25a7a088371def6b7b5e02694cfa482
julioRomero24 <96196210+julioRomero24@users.noreply.github.com>
2022-03-19 20:20:05 -0500

    Add files via upload

---
+++ idioma/lenguaje.py
@@ -0,0 +1,19 @@
+from calendar import c
+import imp
+
+
+
+from idioma import Idioma
+class Lenguaje(Idioma):
+
+    def concatenacion():
+        return 0
+
+    def potencia():
+        return 0
+    
+    def inversa():
+        return 0
+
+    def cardinalidad():
+        return 0
